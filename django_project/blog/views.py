d97dafaddb26768b2d1a7a2560289e1c612b8799
Satya Dileep Penmetsa <satyadileepvarma@gmail.com>
2023-07-27 11:20:34 -0400

    adding database and other related migrations

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        "author":"Satya",
-        "title":"Blog post ",
-        "content":"First Post content",
-        "date_posted":"August 27,2018"
-    },
-    {
-        "author": "Dileep",
-        "title": "Blog post 2",
-        "content": "First Post content",
-        "date_posted": "August 27,2018"
-    }
-]
+from .models import  Post
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return  render(request,"blog/home.html",context)
     # view should always return a http response or an exception
